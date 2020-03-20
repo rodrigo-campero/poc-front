c8b8be2ab4d33d7a25368bc17759cfa49e6092c0
Desenv - Rodrigo Campero Gonçalves <rodrigo.campero@bancodaycoval.com.br>
2020-03-20 18:32:18 -0300

    verificação de objeto dentro da diretica scroll

--- src/app/shared/directives/scroll-to.directive.ts
+++ src/app/shared/directives/scroll-to.directive.ts
@@ -1,14 +1,21 @@
-import { AfterViewInit, Directive, ElementRef } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[scroll-to]'
 })
 export class ScrollToDirective implements AfterViewInit {
+
   constructor(private elRef: ElementRef) { }
+
+  @Input() scrollBlock = 'end';
+
   ngAfterViewInit() {
+    if (!this.elRef.nativeElement.scrollIntoView) {
+      return;
+    }
     this.elRef.nativeElement.scrollIntoView({
       behavior: 'smooth',
-      block: 'end',
+      block: this.scrollBlock,
       inline: 'nearest'
     });
   }
